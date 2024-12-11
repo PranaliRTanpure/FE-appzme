b0fd0d89dde93b5eab252f2e6eea12278e432a23
TTPL-LNVL15-0293 <Pranali Tanpure>
2024-12-11 15:30:50 +0530

    Updated status components

--- src/common-components/status/status.tsx
+++ src/common-components/status/status.tsx
@@ -7,45 +7,47 @@ import { toCamelCase } from "../../utils/toCamelCase";
 import { active } from "./status.widgets";
 
 interface StatusProps {
-	status: string;
-	width?: string;
-	removeStatusBgColor?: boolean;
+  status: string;
+  width?: string;
+  removeStatusBgColor?: boolean;
+  bgColor?: string;
+  textColor?: string;
 }
 
 const Status: React.FC<StatusProps> = (props) => {
-	const { status, width, removeStatusBgColor } = props;
-	const color = status ? StatusColorMap[status] : theme.palette.primary.light;
-	let backgroundColor = "";
-	if (!removeStatusBgColor) {
-		try {
-			backgroundColor = alpha(color, 0.2);
-		} catch {
-			backgroundColor = alpha(theme.palette.primary.light, 0.3);
-		}
-	}
-	const statusTitle = toCamelCase(status || "Undefined");
+  const { status, width, removeStatusBgColor, bgColor, textColor } = props;
+  const color = status ? StatusColorMap[status] : theme.palette.primary.light;
+  let backgroundColor = "";
+  if (!removeStatusBgColor) {
+    try {
+      backgroundColor = bgColor ? bgColor : alpha(color, 0.2);
+    } catch {
+      backgroundColor = alpha(theme.palette.primary.light, 0.3);
+    }
+  }
+  const statusTitle = toCamelCase(status || "Undefined");
 
-	return (
-		<Box
-			sx={{
-				...active,
-				...(removeStatusBgColor
-					? {}
-					: { backgroundColor, width: width ? width : "12rem" }),
-				textAlign: "center",
-			}}
-		>
-			<Typography
-				sx={{
-					fontWeight: "700",
-					fontSize: "0.875rem",
-					color: color || "inherit",
-				}}
-			>
-				{statusTitle}
-			</Typography>
-		</Box>
-	);
+  return (
+    <Box
+      sx={{
+        ...active,
+        ...(removeStatusBgColor
+          ? {}
+          : { backgroundColor, width: width ? width : "12rem" }),
+        textAlign: "center",
+      }}
+    >
+      <Typography
+        sx={{
+          fontWeight: "700",
+          fontSize: "12px",
+          color: textColor ? textColor : color || "inherit",
+        }}
+      >
+        {statusTitle}
+      </Typography>
+    </Box>
+  );
 };
 
 export default Status;
