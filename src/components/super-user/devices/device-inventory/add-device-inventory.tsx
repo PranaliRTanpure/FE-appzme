6aecc4450705a401769c0e4e6c92fe8f7a062bf8
Pranali Tanpure <pranali.tanpure@thinkitive.com>
2024-12-12 12:26:24 +0530

    add device setup

---
+++ src/components/super-user/devices/device-inventory/add-device-inventory.tsx
@@ -0,0 +1,6 @@
+import { Grid } from "@mui/system";
+
+const AddDeviceInventory = () => {
+  return <Grid>hhhhhhiii</Grid>;
+};
+export default AddDeviceInventory;
--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -1,30 +1,32 @@
 import { Grid } from "@mui/system";
 import Navbar from "../common-components/navbar/navbar";
+import React from "react";
 
 const MainLayout = (props: React.PropsWithChildren) => {
-	return (
-		<>
-			<Grid container height={"100vh"}>
-				<Grid
-					container
-					flex={1}
-					flexDirection={"column"}
-					height={"100vh"}
-					sx={{
-						overflow: "auto",
-						maxHeight: "100vh",
-						transition: "all .2s",
-					}}
-					flexWrap={"nowrap"}
-				>
-					<Navbar />
-					<Grid container flex={1} justifyContent={"center"}>
-						{props.children}
-					</Grid>
-				</Grid>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Grid container height={"100vh"}>
+        <Grid
+          container
+          flex={1}
+          flexDirection={"column"}
+          height={"100vh"}
+          sx={{
+            overflow: "auto",
+            maxHeight: "100vh",
+            transition: "all .2s",
+            background: "#F5F6F8",
+          }}
+          flexWrap={"nowrap"}
+        >
+          <Navbar />
+          <Grid container flex={1} justifyContent={"center"}>
+            {props.children}
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default MainLayout;
