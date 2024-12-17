2bfed1010fa66398a5fd12bba90a8c29d444dbb3
Pranali Tanpure <pranali.tanpure@thinkitive.com>
2024-12-17 15:14:47 +0530

    activity log img

--- src/components/super-user/devices/device-inventory/device-inventory-details/active-log.tsx
+++ src/components/super-user/devices/device-inventory/device-inventory-details/active-log.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/system";
+import { Box, Grid } from "@mui/system";
 import { TableHeaders } from "../../../../../common-components/table/table-models";
 import Paginator from "../../../../../common-components/paginator/paginator";
 import {
@@ -16,6 +16,7 @@ import {
   typographyCss,
 } from "../../../../../common-components/table/common-table-widgets";
 import deviceInventoryActivityLogList from "../../../../../mock-data/device-inventory-activeLog.json";
+import Image from "../../../../../assets/image_svg/icons/activity-log-table.svg";
 
 export const mockHeaders: TableHeaders[] = [
   { header: "Date" },
@@ -28,118 +29,155 @@ const ActiveLog = () => {
   return (
     <Grid container width={"100%"} height={"100%"}>
       <Grid container width={"100%"}>
-        <TableContainer
-          sx={{
-            maxHeight: "70vh",
-            overflowY: "scroll",
-          }}
-        >
-          <Table stickyHeader aria-label="sticky table" sx={tableCellCss}>
-            <TableHead>
-              <TableRow>
-                {mockHeaders.map((header, index) => (
-                  <TableCell
-                    sx={{
-                      ...heading,
-                      minWidth: header.minWidth ? header.minWidth : "inherit",
-                      maxWidth: header.maxWidth ? header.maxWidth : "inherit",
-                    }}
-                    align="left"
-                    key={index}
-                  >
-                    <Grid pr={4} container flexDirection={"column"}>
-                      <Typography variant="bodySmall">
-                        {header.header}
+        {deviceInventoryActivityLogList.length > 0 ? (
+          <TableContainer
+            sx={{
+              maxHeight: "70vh",
+              overflowY: "scroll",
+            }}
+          >
+            <Table stickyHeader aria-label="sticky table" sx={tableCellCss}>
+              <TableHead>
+                <TableRow>
+                  {mockHeaders.map((header, index) => (
+                    <TableCell
+                      sx={{
+                        ...heading,
+                        minWidth: header.minWidth ? header.minWidth : "inherit",
+                        maxWidth: header.maxWidth ? header.maxWidth : "inherit",
+                      }}
+                      align="left"
+                      key={index}
+                    >
+                      <Grid
+                        pr={4}
+                        container
+                        flexDirection={"column"}
+                        alignContent={
+                          header.header === "Date" ? "flex-start" : "center"
+                        }
+                      >
+                        <Typography variant="bodySmall">
+                          {header.header}
+                        </Typography>
+                      </Grid>
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {deviceInventoryActivityLogList.length > 0 ? (
+                  deviceInventoryActivityLogList.map((list, index) => (
+                    <TableRow key={index}>
+                      <TableCell sx={{ ...heading }} align="left">
+                        <Grid container flexDirection={"column"}>
+                          <Typography
+                            sx={typographyCss}
+                            variant="bodySmall"
+                            color="#21262B"
+                            fontWeight={400}
+                          >
+                            {list?.date}
+                          </Typography>
+                        </Grid>
+                      </TableCell>
+                      <TableCell sx={{ ...heading }} align="center">
+                        <Grid container flexDirection={"column"}>
+                          <Typography
+                            sx={{ ...typographyCss }}
+                            variant="bodySmall"
+                            color="#21262B"
+                            fontWeight={400}
+                          >
+                            {list?.user}
+                          </Typography>
+                        </Grid>
+                      </TableCell>
+                      <TableCell sx={{ ...heading }} align="center">
+                        <Grid container flexDirection={"column"}>
+                          <Typography
+                            sx={typographyCss}
+                            variant="bodySmall"
+                            color="#21262B"
+                            fontWeight={400}
+                          >
+                            {list?.event}
+                          </Typography>
+                        </Grid>
+                      </TableCell>
+                      <TableCell sx={{ ...heading }} align="left">
+                        <Grid
+                          container
+                          flexDirection={"column"}
+                          alignContent={"center"}
+                        >
+                          <Typography
+                            sx={{ ...typographyCss }}
+                            variant="bodySmall"
+                            color="#21262B"
+                            fontWeight={400}
+                          >
+                            {list?.appointment}
+                          </Typography>
+                        </Grid>
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={9} align="center">
+                      <Typography variant="bodySmall" fontWeight={550}>
+                        No records found
                       </Typography>
-                    </Grid>
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {deviceInventoryActivityLogList.length > 0 ? (
-                deviceInventoryActivityLogList.map((list, index) => (
-                  <TableRow key={index}>
-                    <TableCell sx={{ ...heading }} align="left">
-                      <Grid container flexDirection={"column"}>
-                        <Typography
-                          sx={typographyCss}
-                          variant="bodySmall"
-                          color="#21262B"
-                          fontWeight={400}
-                        >
-                          {list?.date}
-                        </Typography>
-                      </Grid>
-                    </TableCell>
-                    <TableCell sx={{ ...heading }} align="left">
-                      <Grid container flexDirection={"column"}>
-                        <Typography
-                          sx={{ ...typographyCss }}
-                          variant="bodySmall"
-                          color="#21262B"
-                          fontWeight={400}
-                        >
-                          {list?.user}
-                        </Typography>
-                      </Grid>
-                    </TableCell>
-                    <TableCell sx={{ ...heading }} align="left">
-                      <Grid container flexDirection={"column"}>
-                        <Typography
-                          sx={typographyCss}
-                          variant="bodySmall"
-                          color="#21262B"
-                          fontWeight={400}
-                        >
-                          {list?.event}
-                        </Typography>
-                      </Grid>
-                    </TableCell>
-                    <TableCell sx={{ ...heading }} align="left">
-                      <Grid container flexDirection={"column"}>
-                        <Typography
-                          sx={{ ...typographyCss }}
-                          variant="bodySmall"
-                          color="#21262B"
-                          fontWeight={400}
-                        >
-                          {list?.appointment}
-                        </Typography>
-                      </Grid>
                     </TableCell>
                   </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={9} align="center">
-                    <Typography variant="bodySmall" fontWeight={550}>
-                      No records found
-                    </Typography>
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <Grid
+            container
+            width={"100%"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            rowGap={1}
+            height={356}
+          >
+            <Box width={210} height={176} component={"img"} src={Image}></Box>
+            <Typography variant="bodyMedium" fontWeight={500}>
+              No Logs Available
+            </Typography>
+            <Typography
+              variant="bodySmall"
+              fontWeight={400}
+              sx={{ color: "#9B9D9F" }}
+            >
+              Select a date range to view activity logs.
+            </Typography>
+          </Grid>
+        )}
       </Grid>
-      <Grid
-        p={1}
-        width={"100%"}
-        container
-        sx={{ borderTop: "1px solid #E7E7E7" }}
-      >
-        <Paginator
-          page={0}
-          totalPages={5}
-          totalRecord={5}
-          onPageChange={() => {}}
-          onRecordsPerPageChange={() => {}}
-          defaultSize={10}
-        />
-      </Grid>
+      {/* Pagination */}
+      {deviceInventoryActivityLogList.length > 0 && (
+        <Grid
+          p={1}
+          width={"100%"}
+          container
+          sx={{ borderTop: "1px solid #E7E7E7" }}
+        >
+          <Paginator
+            page={0}
+            totalPages={5}
+            totalRecord={5}
+            onPageChange={() => {}}
+            onRecordsPerPageChange={() => {}}
+            defaultSize={10}
+          />
+        </Grid>
+      )}
     </Grid>
   );
 };
-
 export default ActiveLog;
